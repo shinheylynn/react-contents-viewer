8b26e0d46d916a2821c0720078468de51c023902
shinheylynn <heylub@gmail.com>
2024-03-17 00:24:42 +0900

    :art: Add: Footer.jsx 맨 위로 올라가기 버튼
    최하단에 맨 위로 올라가기 버튼 기능 추가

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 const FooterContainer = styled.div``;
-const GoToTopButton = styled.button``;
+
+const ChevronUp = styled(FontAwesomeIcon)`
+  font-size: 20px;
+  color: var(--grey);
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 const Footer = () => {
+  const MoveToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <FooterContainer>
-      <GoToTopButton>^</GoToTopButton>
+      <ChevronUp icon={faCircleChevronUp} onClick={MoveToTop}>
+        ^
+      </ChevronUp>
     </FooterContainer>
   );
 };
